ac636d1b7eade9ef937ea4a8d65fc48e3697b2a1
Sanshung <shakleinserg@gmail.com>
2018-04-05 11:23:00 +0600

    Create script.php

---
+++ script.php
@@ -0,0 +1,24 @@
+<?
+\Bitrix\Main\Loader::includeModule('sale');
+
+$orderData = " 2018-04-04T19:00:46";
+$orderCheck = "test 1232-".time();
+
+$order = \Bitrix\Sale\Order::Load(112004);
+
+
+$order->setField("STATUS_ID", "CL");
+$order->setField('COMMENTS', 'Заказ был закрыт автоматически');
+
+
+$paymentCollection = $order->getPaymentCollection();
+foreach ($paymentCollection as $payment) {
+    $payment->setFields(array(
+        'PAY_VOUCHER_NUM' => $orderCheck,
+        'PAY_VOUCHER_DATE' => \Bitrix\Main\Type\DateTime::createFromUserTime(date('d.m.Y H:i:s', strtotime($orderData))),
+    ));
+    $p = $payment->setPaid('Y');
+    $payment->save();
+}
+$order->save();
+?>
